d1ba685bf7204993ef38f68e71370bd20d97580b
AnnaGrigoryan86 <53938112+AnnaGrigoryan86@users.noreply.github.com>
2019-09-27 04:02:25 +0400

    Update myPage.js

--- myPage.js
+++ myPage.js
@@ -1,1 +1,9 @@
-
+function welcome(){
+    let answer = prompt("Are you sure you want to access my page?");
+    if(answer == "yes") {
+        alert("Welcome!");
+    } else {
+        alert("Sorry, your access is closed(((");
+        welcome();
+    }
+}
